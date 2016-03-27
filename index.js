a0ef3871c9be17e01a1ae1557fd5e71ec0c505c7
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2016-03-27 10:07:10 +0800

    renamed and removed

--- index.js
+++ index.js
@@ -1,95 +1,17 @@
-/**
- * Module dependencies.
- */
 
-var express = require('../..');
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require('express');
+var app = express();
 
-var app = module.exports = express();
-
-// settings
-
-// set our default template engine to "jade"
-// which prevents the need for extensions
-app.set('view engine', 'pug');
-
-// set views for error and 404 pages
-app.set('views', __dirname + '/views');
-
-// define a custom res.message() method
-// which stores messages in the session
-app.response.message = function(msg){
-  // reference `req.session` via the `this.req` reference
-  var sess = this.req.session;
-  // simply add the msg to an array for later
-  sess.messages = sess.messages || [];
-  sess.messages.push(msg);
-  return this;
-};
-
-// log
-if (!module.parent) app.use(logger('dev'));
-
-// serve static files
-app.use(express.static(__dirname + '/public'));
-
-// session support
-app.use(session({
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored
-  secret: 'some secret here'
-}));
-
-// parse request bodies (req.body)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// allow overriding methods in query (?_method=put)
-app.use(methodOverride('_method'));
-
-// expose the "messages" local variable when views are rendered
-app.use(function(req, res, next){
-  var msgs = req.session.messages || [];
-
-  // expose "messages" local variable
-  res.locals.messages = msgs;
-
-  // expose "hasMessages"
-  res.locals.hasMessages = !! msgs.length;
-
-  /* This is equivalent:
-   res.locals({
-     messages: msgs,
-     hasMessages: !! msgs.length
-   });
-  */
-
-  next();
-  // empty or "flush" the messages so they
-  // don't build up
-  req.session.messages = [];
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-// load controllers
-require('./lib/boot')(app, { verbose: !module.parent });
-
-app.use(function(err, req, res, next){
-  // log it
-  if (!module.parent) console.error(err.stack);
-
-  // error page
-  res.status(500).render('5xx');
+app.get('/about', function (req, res) {
+  res.send('Hello World!');
 });
 
-// assume 404 since no middleware responded
-app.use(function(req, res, next){
-  res.status(404).render('404', { url: req.originalUrl });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
-/* istanbul ignore next */
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
-}
